3cd22100bee55f29ad447185ebea00af5bd946c4
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2024-07-19 14:01:04 +0300

    register and login in the backend all setted up

--- ReactDjangoNote/backend/NoteManager/notes/serializers.py
+++ ReactDjangoNote/backend/NoteManager/notes/serializers.py
@@ -1,2 +1,15 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username","email", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
+        )
+        return user
