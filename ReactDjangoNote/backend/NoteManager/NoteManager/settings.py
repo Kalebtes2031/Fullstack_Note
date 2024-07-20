986fc262031062e3f4f6fb1d03a163de31041aad
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2024-07-20 10:35:42 +0300

    Update settings.py

--- ReactDjangoNote/backend/NoteManager/NoteManager/settings.py
+++ ReactDjangoNote/backend/NoteManager/NoteManager/settings.py
@@ -158,7 +158,9 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://fullstack-note-frontend.vercel.app',
+]
 CORS_ALLOW_CREDENTIALS = True
 
 # settings.py
