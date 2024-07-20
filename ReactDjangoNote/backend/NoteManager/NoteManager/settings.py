4b1d113f2ccb54d60b46b75041ac2d9f4c6b0783
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2024-07-20 09:36:03 +0300

    update the url to deployed urls

--- ReactDjangoNote/backend/NoteManager/NoteManager/settings.py
+++ ReactDjangoNote/backend/NoteManager/NoteManager/settings.py
@@ -15,7 +15,7 @@ from datetime import timedelta
 from dotenv import load_dotenv
 import os
 import dj_database_url
-from decouple import config
+
 
 
 load_dotenv()
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-&8kwxh!n74t5=roc%g3!%dtny(25kjb2el*wdtt2cud7m1r-!s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app","127.0.0.1","localhost",".now.sh"]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
@@ -98,14 +98,22 @@ WSGI_APPLICATION = 'NoteManager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': "railway",
+#         'USER': 'postgres',
+#         'PASSWORD':'OzXdbsphAyBgDcLcKtYpUtYePNuVSIHD',
+#         'HOST':'postgres.railway.internal',
+#         'PORT':'5432',
+#     }
+# }
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -142,6 +150,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
